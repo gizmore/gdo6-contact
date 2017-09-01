d02a414bc7f19b050a486d0db6c180c587ad3f31
gizmore <gizmore@wechall.net>
2017-09-02 01:17:30 +0200

    detect_lang_and_sync

---
+++ lang/contact_de.php
@@ -0,0 +1,28 @@
+<?php
+return array(
+'link_contact' => 'Kontakt',
+'link_message' => 'Show',
+######################################
+'ft_contact_form' => '[%s] Contact',
+'msg_contact_mail_sent' => 'Your message has been sent to the administrators of %s.',
+######################################
+'card_title_contact_message' => '[%s] Contact Message',
+'msg_by' => 'Message from: %s',
+'msg_title' => 'Title: %s',
+######################################
+'mail_subj_contact' => '[%s] Contact Mail',
+'mail_body_contact' => '
+Dear %s,
+
+The contact form on %s has been used and a message has been sent.
+
+From: %s <%s>
+
+Title: %s
+<hr/>
+%s
+<hr/>
+Kind Regards
+The %2$s Team',
+######################################
+);
